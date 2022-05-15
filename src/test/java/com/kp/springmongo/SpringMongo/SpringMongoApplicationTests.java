c94d2b0cbdd7a21e9ca03f502099bf625b3c745d
kp <karthik.hy004@gmail.com>
2022-05-16 02:52:25 +0530

    Initial commit

---
+++ src/test/java/com/kp/springmongo/SpringMongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.springmongo.SpringMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
